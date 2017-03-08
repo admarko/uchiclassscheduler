c579baf0b94b96bb4a53c1af80bd4ccd425632de
Raghu Betina <raghu@firstdraft.com>
2017-03-08 21:59:38 +0000

    Direct association between users and schedules

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,6 +1,10 @@
 class Schedule < ApplicationRecord
   # Direct associations
 
+  belongs_to :student,
+             :class_name => "User",
+             :foreign_key => "user_id"
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :schedules,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
